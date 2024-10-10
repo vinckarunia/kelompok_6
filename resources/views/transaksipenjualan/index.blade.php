40018d904cacc4a61b86397b10e3a5ac7a1e66e8
mjayy77 <billymj77@gmail.com>
2024-10-10 23:39:28 +0700

    modified:   resources/views/transaksipenjualan/index.blade.php

--- resources/views/transaksipenjualan/index.blade.php
+++ resources/views/transaksipenjualan/index.blade.php
@@ -4,61 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Transaction Data</title>
+    <title>Data Transaksi Penjualan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body style="background: rgb(0, 0, 0)">
+<body style="background: lightgray">
 
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <h3 class="text-center my-4">ANJAYANI SHOP TRANSACTION</h3>
+                    <h3 class="text-center my-4">TRANSAKSI PENJUALAN</h3>
                     <hr>
                 </div>
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
-                        <a href="{{ route('transaksipenjualan.create') }}" class="btn btn-md btn-success mb-3">ADD TRANSACTION</a>
+                        <a href="{{ route('transaksi.create') }}" class="btn btn-md btn-success mb-3">ADD TRANSACTION</a>
                         <table class="table table-bordered">
                             <thead>
                                 <tr>
-                                    <th scope="col">ID PRODUK</th>
-                                    <th scope="col">JUMLAH PEMBELIAN</th>
-                                    <th scope="col">NAMA KASIR</th>
                                     <th scope="col">TANGGAL TRANSAKSI</th>
+                                    <th scope="col">NAMA KASIR</th>
+                                    <th scope="col">NAMA PRODUK</th>
+                                    <th scope="col">KATEGORI PRODUK</th>
+                                    <th scope="col">HARGA</th>
+                                    <th scope="col">JUMLAH</th>
+                                    <th scope="col">TOTAL HARGA</th>
                                     <th scope="col" style="width: 20%">ACTIONS</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @forelse ($transactions as $transaction)
+                                @forelse ($transaksi as $transaction)
                                 <tr>
-                                    <td>{{ $transaction->id_products }}</td>
-                                    <td>{{ $transaction->jumlah_pembelian }}</td>
+                                <td>{{ \Carbon\Carbon::parse($transaction->tanggal_transaksi)->format('d-m-Y') }}</td>
                                     <td>{{ $transaction->nama_kasir }}</td>
-                                    <td>{{ $transaction->tanggal_transaksi }}</td>
+                                    <td>{{ $transaction->nama_produk }}</td>
+                                    <td>{{ $transaction->kategori_produk }}</td>
+                                    <td>{{ "Rp " . number_format($transaction->harga, 2, ',', '.') }}</td>
+                                    <td>{{ $transaction->jumlah_pembelian }}</td>
+                                    <td>{{ "Rp " . number_format($transaction->total_harga, 2, ',', '.') }}</td>
                                     <td class="text-center">
-                                        <form onsubmit="return confirm('Apakah Anda Yakin ?')" action="{{ route('transaksipenjualan.destroy', $transaction->id) }}" method="POST">
-                                            <a href="{{ route('transaksipenjualan.show', $transaction->id) }}" class="btn btn-sm btn-dark">SHOW</a>
-                                            <a href="{{ route('transaksipenjualan.edit', $transaction->id) }}" class="btn btn-sm btn-primary">EDIT</a>
-                                            @csrf
-                                            @method('DELETE')
+                                    <form onsubmit="return confirm('Apakah Anda Yakin ?')" action="{{ route('transaksi.destroy', $transaction->id) }}" method="POST">
+                                        <a href="{{ route('transaksi.show', $transaction->id) }}" class="btn btn-sm btn-dark">SHOW</a>
+                                        <a href="{{ route('transaksi.edit', $transaction->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                    </form>
 
-                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
-                                        </form>
                                     </td>
                                 </tr>
                                 @empty
                                 <tr>
-                                    <td colspan="5" class="text-center">
+                                    <td colspan="8" class="text-center">
                                         <div class="alert alert-danger">
-                                            Data Transaksi belum Tersedia.
+                                            Data Transaksi Penjualan belum Tersedia.
                                         </div>
                                     </td>
                                 </tr>
                                 @endforelse
                             </tbody>
                         </table>
-                        {{ $transactions->links() }}
+                        {{ $transaksi->links() }}
                     </div>
                 </div>
             </div>
@@ -69,7 +75,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
-        // Message with SweetAlert
+        // message with sweetalert
         @if(session('success'))
             Swal.fire({
                 icon: 'success',
