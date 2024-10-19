59bf6a86b2d4b5ff10ae2ecfa5fda4c5c9f592c4
vinckarunia <vinckarunia@gmail.com>
2024-10-20 00:23:59 +0700

    CSS for create and edit

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -6,12 +6,77 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit Products</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background: linear-gradient(to right, darkslateblue, salmon);
+        }
+        
+        .card {
+            background: linear-gradient(to right, rgba(231, 243, 254, 0.3), rgba(255, 255, 255, 0.3));
+            border-radius: 10px;
+            box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h4 {
+            font-size: 24px;
+            font-weight: bold;
+            color: #ffffff;
+            margin-bottom: 20px;
+        }
+
+        label {
+            font-weight: bold;
+            color: #ffffff;
+        }
+
+        .form-control {
+            border: 1px solid #ced4da;
+            padding: 10px;
+            font-size: 14px;
+        }
+
+        .btn-primary {
+            background-color: #6A5ACD;
+            border-color: #007bff;
+            color: #FFFFFF;
+            font-weight: bold;
+            padding: 10px 20px;
+            border: none;
+        }
+
+        .btn-primary:hover {
+            background-color: #4B0082;
+            color: #FFFFFF;
+            border: none;
+        }
+
+        .btn-warning {
+            background-color: #FF7F50;
+            border-color: #ffc107;
+            color: #FFFFFF;
+            font-weight: bold;
+            padding: 10px 20px;
+            border: none;
+        }
+
+        .btn-warning:hover {
+            background-color: #FF6347;
+            color: #FFFFFF;
+            border: none;
+        }
+
+        .alert-danger {
+            font-size: 12px;
+            padding: 5px;
+            margin-top: 10px;
+        }
+    </style>
+
 </head>
-<body style="background: lightgray">
     <div class="container mt-5 mb-5">
         <div class="row">
             <div class="col-md-12">
-                <h4>Edit Product</h4>
+                <h4 class="title">Edit Product</h4>
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
                         <form action="{{ route('products.update', $data['product']->id) }}" method="POST" enctype="multipart/form-data">
@@ -134,8 +199,16 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+    <script src="https://unpkg.com/typeit@8.7.1/dist/index.umd.js"></script>
     <script>
         CKEDITOR.replace('description');
+
+        document.addEventListener("DOMContentLoaded", function () {
+            new TypeIt(".title", {
+            strings: [],
+            speed: 50
+            }).go();
+        });
     </script>
 </body>
 </html>
