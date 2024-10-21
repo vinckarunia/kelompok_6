aa04eb27343a9ae660d016fcd972074149dc8ccb
vinckarunia <vinckarunia@gmail.com>
2024-10-21 22:18:19 +0700

    Merge feliciano_422023006 to main

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,28 +26,3 @@ class AppServiceProvider extends ServiceProvider
     Schema::defaultStringLength(191);
     }
 }
-
-<?php
-
-namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
-}
